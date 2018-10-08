71be581cb4ea89a447d59b01f5317d77c815bd57
seanbrittian <39929810+seanbrittian@users.noreply.github.com>
2018-10-08 04:29:10 -0400

    sqlliterepair

--- jobs/app.py
+++ jobs/app.py
@@ -6,7 +6,7 @@ PATH = 'db/job.sqlite'
 app = Flask (__name__)
 
 def open_connection():
-    connection = getattr(g, '_connection, None')
+    connection = getattr(g, '_connection', None)
     if connection ==None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
